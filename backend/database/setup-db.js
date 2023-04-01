00c2a3d0d61c621a9205ecb7ad3938553ba3cbff
Georgi Kolev <g.t.kolev1@gmail.com>
2023-04-01 23:53:19 +0300

    Fixed database bugs and added registration

--- backend/database/setup-db.js
+++ backend/database/setup-db.js
@@ -1,19 +1,42 @@
 const Datastore = require('nedb');
+const bcrypt = require('bcrypt');
+const users = new Datastore({filename: './database/users.db', autoload: true});
+const matches = new Datastore({filename: './database/matches.db', autoload: true});
 
-function create_db() {
-    var db = new Datastore({filename: './database/test.db', autoload: true});
-
-    db.insert({name: 'TestName', testMessage: 'Hellooo'}, function (err, doc) {
-        if (err) {
-            console.log('Error inserting user: ', err);
-        } else {
-            console.log('User inserted successfully: ', doc);
-        }
-    });
-}
 function seed_football_matches() {
-    var matches = new Datastore({filename: './database/matches.db', autoload: true});
-    matches.insert({ homeTeam: 'Man Utd', homeTeamLogo: 'TODO: add link', awayTeam: 'Man City', awaiTeamLogo: 'TODO: add link'})
+    matches.insert({ event: 'Premier League', homeTeam: 'Man Utd', homeTeamLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png', awayTeam: 'Man City', awaiTeamLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png', startTime: new Date(2023, 1, 5, 19, 0)});
+    matches.insert({ event: 'Premier League', homeTeam: 'Liverpool', homeTeamLogo: 'TODO: add link', awayTeam: 'Brighton', awaiTeamLogo: 'TODO: add link', startTime: new Date(2023, 1, 5, 22, 0)});
+    matches.insert({ event: 'La Liga', homeTeam: 'Barcelona', homeTeamLogo: 'TODO: add link', awayTeam: 'Real Madrid', awaiTeamLogo: 'TODO: add link', startTime: new Date(2023, 1, 5, 21, 45)});
+    matches.insert({ event: 'Seria A', homeTeam: 'Juventus', homeTeamLogo: 'TODO: add link', awayTeam: 'Inter', awaiTeamLogo: 'TODO: add link', startTime: new Date(2023, 1, 6, 18, 0)});
+    matches.insert({ event: 'FA Cup', homeTeam: 'Chelsea', homeTeamLogo: 'TODO: add link', awayTeam: 'Arsenal', awaiTeamLogo: 'TODO: add link', startTime: new Date(2023, 1, 7, 19, 30)});
 }
 
-module.exports = { create_db };
+function register(username, password) {
+    if (!doesUserExists(username, password)) {
+        users.insert({ username: username, password: password });
+    }
+}
+
+function doesUserExists(username, password) {
+    for (let user in users) {
+        //TODO: Check for password
+        if (user.username == username) {
+            return user;
+        }
+    }
+}
+
+const hashPassword = async password => {
+    try {
+        const salt = await bcrypt.genSalt(12);
+        return await bcrypt.hash(password, salt);
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
+
+function getAllMatches() {
+    return matches.getAllData();
+}
+
+module.exports = { seed_football_matches, register, doesUserExists, getAllMatches };
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -7,7 +7,7 @@ function Login() {
     const navigate = useNavigate();
 
     const navigateToLayout= () => {
-        navigate('Layout');
+        // navigate('Layout');
     }
 
     return (
