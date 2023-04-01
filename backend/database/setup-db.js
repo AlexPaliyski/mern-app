9ca6d35d62d363fba01228d79b351054686308ec
Georgi Kolev <g.t.kolev1@gmail.com>
2023-04-01 16:33:13 +0300

    Test dba and added seeder for football matches (not finished)

--- backend/database/setup-db.js
+++ backend/database/setup-db.js
@@ -1,9 +1,19 @@
-import * as nedb from 'nedb';
+const Datastore = require('nedb');
+
 function create_db() {
-    var Datastore = require('nedb')
-        , db = new Datastore();
-    db.loadDatabase(function (err) {    // Callback is optional
-        // Now commands will be executed
+    var db = new Datastore({filename: './database/test.db', autoload: true});
+
+    db.insert({name: 'TestName', testMessage: 'Hellooo'}, function (err, doc) {
+        if (err) {
+            console.log('Error inserting user: ', err);
+        } else {
+            console.log('User inserted successfully: ', doc);
+        }
     });
 }
-export default create_db();
+function seed_football_matches() {
+    var matches = new Datastore({filename: './database/matches.db', autoload: true});
+    matches.insert({ homeTeam: 'Man Utd', homeTeamLogo: 'TODO: add link', awayTeam: 'Man City', awaiTeamLogo: 'TODO: add link'})
+}
+
+module.exports = { create_db };
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { create_db } = require('./database/setup-db');
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
@@ -10,4 +11,5 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-console.log('passed from here')
+console.log("Passed from here");
+create_db();
